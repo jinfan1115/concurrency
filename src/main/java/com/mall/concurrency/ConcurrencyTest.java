6859bb61a4a8c348e2be92865d0b33e1110df5ce
jinfan1115@outlook.com <jinfan1115@outlook.com>
2018-04-28 22:57:41 +0800

    add ConcurrencyTest

---
+++ src/main/java/com/mall/concurrency/ConcurrencyTest.java
@@ -0,0 +1,43 @@
+package com.mall.concurrency;
+
+import com.mall.concurrency.annoations.NoThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+@NoThreadSafe
+public class ConcurrencyTest {
+    //运行次数
+    private  static int clientTotal = 5000;
+    //线程总数
+    private  static int threadTotal = 200;
+
+    private  static  int count = 0;
+
+    public static void main (String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try{
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception ex){
+                    log.error("exception", ex);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        executorService.shutdown();
+        countDownLatch.await();
+        log.info("count : " + count);
+    }
+
+    private static void add(){
+        count ++;
+    }
+
+}
--- src/test/java/com/mall/concurrency/ConcurrencyTestApplicationTests.java
+++ src/test/java/com/mall/concurrency/ConcurrencyTestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConcurrencyApplicationTests {
+public class ConcurrencyTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
